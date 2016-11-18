3480abf1789759fd2d2c1f52c69492f1abe971a0
JonnyNan <nanzhang1211@look.com>
2016-11-18 16:01:34 +0800

    发布会管理系统

---
+++ sign/views.py
@@ -0,0 +1,31 @@
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        auth.login(request, user)  # 登录
+        # response.set_cookie('user', username, 3600)  # 添加浏览器cookie
+        request.session['user'] = username
+        response = HttpResponseRedirect('/event_manage/')
+        return response
+    else:
+        return render(request, 'index.html', {'error': 'username or password error!'})
+
+@login_required
+def event_manage(request):
+    # username = request.COOKIES.get('user', '')  # 读取浏览器cookie
+    username = request.session.get('user', '')
+    return render(request, "event_manage.html", {"user": username})
